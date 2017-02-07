21b24ef6d852d2248ebbab1b1c5e1896bc4ee9e9
Kastriot Kosumi <mr.kosumi@gmail.com>
2017-02-07 21:28:57 +0100

    added api with some methods

---
+++ app/Controllers/API/VideosController.cs
@@ -0,0 +1,80 @@
+﻿using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace app.Controllers.API
+{
+    public class VideosController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public VideosController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET /api/videos
+        public IEnumerable<Video> GetVideos()
+        {
+            return _context.Videos.ToList();
+        }
+        //GET /api/videos/1
+        public Video GetVideo(int id)
+        {
+            var video = _context.Videos.SingleOrDefault(v => v.Id == id);
+
+            if(video == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return video;
+        }
+        // POST /api/videos
+        [HttpPost]
+        public Video CreateVideo(Video video)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _context.Videos.Add(video);
+            _context.SaveChanges();
+
+            return video;
+        }
+
+        // PUT /api/videos/1
+        [HttpPut]
+        public void UpdateVideo(int id, Video video)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var videodb = _context.Videos.SingleOrDefault(v => v.Id == id);
+
+            if (video == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            videodb.Title = video.Title;
+            videodb.Description = video.Description;
+            videodb.Genre = video.Genre;
+            _context.SaveChanges();
+        }
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var video = _context.Videos.SingleOrDefault(v => v.Id == id);
+
+            if (video == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Videos.Remove(video);
+            _context.SaveChanges();
+        }
+    }
+}
