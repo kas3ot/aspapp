8671c8cc9b189d44dca1f41c84242b892f85ad27
kas3ot <mr.kosumi@gmail.com>
2017-02-07 10:25:58 +0100

    first commit

---
+++ app/Controllers/VideosController.cs
@@ -0,0 +1,126 @@
+﻿using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app.Controllers
+{
+    public class VideosController : Controller
+    {
+        
+        private ApplicationDbContext _dbContext;
+
+        public VideosController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Videos
+        public ActionResult Index()
+        {
+            var videos = _dbContext.Videos.OrderByDescending(id => id.Id).ToList();
+
+            return View(videos);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Add(Video video)
+        {
+            _dbContext.Videos.Add(video);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (Request.IsAuthenticated)
+            { 
+                var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+
+                if (video == null)
+                    return HttpNotFound();
+
+                return View(video);
+            }
+            return RedirectToAction("../Account/Login");
+        }
+
+        [HttpPost]
+        public ActionResult Update(Video video)
+        {
+            if (Request.IsAuthenticated)
+            {
+                var videoInDb = _dbContext.Videos.SingleOrDefault(v => v.Id == video.Id);
+
+                if (videoInDb == null)
+                    return HttpNotFound();
+
+                videoInDb.Title = video.Title;
+                videoInDb.Description = video.Description;
+                videoInDb.Genre = video.Genre;
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("../Account/Login");
+
+        }
+  
+        public ActionResult Delete(int id)
+        {
+            if (Request.IsAuthenticated)
+            {
+                var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+
+                if (video == null)
+                    return HttpNotFound();
+
+                return View(video);
+            }
+
+            return RedirectToAction("../Account/Login");
+        }
+
+        public ActionResult DoDelete(int id)
+        {
+            if (Request.IsAuthenticated)
+            {
+                var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+
+                if (video == null)
+                    return HttpNotFound();
+                _dbContext.Videos.Remove(video);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("../Account/Login");
+        }
+
+        public ActionResult Search(string search)
+        {
+            var videos = _dbContext.Videos.OrderByDescending(id => id.Id).Where(s => s.Title == search).ToList();
+            return View(videos);
+        }
+
+        public ActionResult Genre(Genre genre)
+        {
+            var videos = _dbContext.Videos.OrderByDescending(id => id.Id).Where(s => s.Genre == genre).ToList();
+            return View(videos);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var vid = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+            return View(vid);
+        }
+    }
+}
---
+++ app/Models/Video.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Genre Genre { get; set; }
+    }
+
+    public enum Genre
+    {
+        Comedy = 1,
+        Horror,
+        Video_tutorials,
+        SciFi,
+        Romance,
+        Documentary,
+        Kids
+    }
+}
